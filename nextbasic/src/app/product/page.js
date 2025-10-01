8afa13a73ea9ca869c4f304121680ce02e3d1a7a
santosh patel <santoshpatelvns5@gmail.com>
2025-10-01 19:16:42 +0530

    dynamic routing

---
+++ nextbasic/src/app/product/page.js
@@ -0,0 +1,13 @@
+export default function product(){
+    return(
+        <div style={{textAlign:"center"} }>
+            <ul style={{listStyleType:"none"}}>
+                <li style={{}}>product1</li>
+                <li>product2</li>
+                <li>product3</li>
+                <li>product4</li>
+                <li>product5</li>
+            </ul>
+        </div>
+    )
+}
